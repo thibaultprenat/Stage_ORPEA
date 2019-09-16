bc0175bfca4a99b039e2e9505a648e95df4d1b67
BidaultMathis <mathis.bidault4@gmail.com>
2019-09-17 00:11:10 +0200

    Ajout classes

---
+++ Sources/Planification/PlanifActivite.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Orpea_WF.Activite.Planification
+{
+    /// <summary>
+    ///  Stockage des infos de l'activité
+    /// </summary>
+
+    public class Activité
+    {
+        public int IdActivite             { get; }
+        public DateTime DateDebutActivite { get; }
+
+        // Constructeur de la classe Activité
+        public Activité(int idActivite, DateTime dateDebutActivite)
+        {
+            IdActivite        = idActivite;
+            DateDebutActivite = dateDebutActivite;
+        }
+    }
+
+    public class Planification
+    {
+        /// <summary>
+        ///  Accès aux propriétés de la classe Activité
+        /// </summary>
+        private Activité _Activité { get; }
+
+        // Constructeur
+        public Planification(Activité activité)
+        {
+            _Activité = activité;
+        }
+
+        /// <summary>
+        /// Exécution de la requête.
+        /// Cette requête paramétrée ajoute une activité au planning. 
+        /// </summary>
+        public string AjoutActivitePlanning()
+        {
+            try
+            {
+                using (var mySqlConnection = new MySqlConnection())
+                {
+                    mySqlConnection.ConnectionString = Utilisateur.Connexion.SqlConnexion;
+                    mySqlConnection.Open();
+
+                    const string query = "INSERT INTO PLANNING (PLAN_ACTIVITE, PLAN_DATE_DEBUT) " +
+                                         "VALUES (@PLAN_ACTIVITE, @PLAN_DATE_DEBUT)";
+
+                    using (var mySqlCommand = new MySqlCommand(query, mySqlConnection))
+                    {
+                        mySqlCommand.Parameters.Add(new MySqlParameter("@PLAN_ACTIVITE",   MySqlDbType.Int16));
+                        mySqlCommand.Parameters["@PLAN_ACTIVITE"].Value   = Convert.ToInt16(_Activité.IdActivite);
+                        mySqlCommand.Parameters.Add(new MySqlParameter("@PLAN_DATE_DEBUT", MySqlDbType.DateTime));
+                        mySqlCommand.Parameters["@PLAN_DATE_DEBUT"].Value = _Activité.DateDebutActivite.ToString("G", CultureInfo.CreateSpecificCulture("zh-CN"));
+
+                        var result = mySqlCommand.ExecuteNonQuery();
+
+                        return result > 0 ? "1" : "0";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return MessageBox.Show($@"Erreur : {ex.Message}", @"Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation).ToString();
+            }
+        }
+    }
+}
+
---
+++ Sources/Utilisateur/Connexion.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using Orpea_WF.Cryptographie;
+
+namespace Orpea_WF.Utilisateur
+{
+
+    /// <summary>
+    ///  Cette classe gère l'accès (connexion) de l'utilisateur à l'application. La particularité de cette classe ainsi que de toutes les autres
+    ///  est que la requête est divisée en deux : une partie requête + paramètres et une autre qui gère l'exécution de la requête
+    /// </summary>
+
+    public class Connexion
+    {
+        /// <summary>
+        ///  Fonction principale : Stockage de l'Id utilisateur entré dans la textBox.
+        ///  Fonction secondaire : Permet l'affichage de l'utilisateur connecté à l'application.
+        /// </summary>
+        public string   IdUtilisateur    { get; set; }
+
+        /// <summary>
+        ///  Fonction principale : Stockage du mot de passe utilisateur entré dans la textBox.
+        /// </summary>
+        public string   MdpUtilisateur { private get; set; }
+        public string   MsgException   { get; private set; }
+
+        private string _mdpUtilisateurCrypte;
+        private bool   _compteExistant;
+
+        /// <summary>
+        ///  Fonction principale : Chaîne de connexion à la base de données réutilisée pour toutes les autres requêtes.
+        /// </summary>
+        public const string SqlConnexion = "server=mysql-orpeafauriel.alwaysdata.net;Port=3306; userid=180881; password=Rdc3ycww**; database=orpeafauriel_bdd";
+
+        /// <summary>
+        ///  Fonction principale : Cette méthode prépare la requête, tente de se connecter à la BDD, crée les paramètres puis exécute le return.
+        /// </summary>
+        /// <returns>Exécute la méthode "AuthentificationExection(MySqlCommand mySqlCommand)".</returns>
+        /// <exception cref="Exception">L'exception peut être déclenchée si la requête plante, si la connexion à la BDD est impossible etc...</exception>
+        public string Authentification()
+        {
+            try
+            {
+                using (var mySqlConnection = new MySqlConnection())
+                {
+                    mySqlConnection.ConnectionString = SqlConnexion;
+                    mySqlConnection.Open();
+
+                    const string query = "SELECT * FROM UTILISATEURS WHERE USER_ID=@U_ID";
+
+                    using (var mySqlCommand = new MySqlCommand(query, mySqlConnection))
+                    {
+                        mySqlCommand.Parameters.Add(new MySqlParameter("@U_ID", MySqlDbType.VarChar));
+                        mySqlCommand.Parameters["@U_ID"].Value = IdUtilisateur;
+
+                        return AuthentificationExection(mySqlCommand);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MsgException = exception.Message;
+                return "2";
+            }
+        }
+
+        /// <summary>
+        ///  Fonction principale : Cette méthode exécute la requête en fonction de ses paramètres puis retourne son résultat.
+        ///  A savoir que les mots de passe sont chiffrés de base puis déchiffrés ci-dessous !
+        /// </summary>
+        /// <param name="mySqlCommand">Paramètres (Nom d'utilisateur, mot de passe) à exécuter pour la requête.</param>
+        /// <returns>Retourne le résultat de la requête. Si le résultat est = 1 (Déchiffrage réussi) alors stockage de l'Id utilisateur puis autorisation de la connexion.</returns>
+        private string AuthentificationExection(MySqlCommand mySqlCommand)
+        {
+            using (var mySqlDataReader = mySqlCommand.ExecuteReader())
+            {
+                if (mySqlDataReader.Read())
+                {
+                    // Récupération du mot de passe chiffré.
+                    _mdpUtilisateurCrypte = mySqlDataReader.GetString(1);
+                    _compteExistant       = true;
+                }
+
+                // Si le compte existe alors on procède à la tentative de déchiffrement du mot de passe
+                // S'il échoue on retourne 0 le mot de passe est incorrect, sinon on retourne 1
+                if (_compteExistant)
+                {
+                    return AesDecryption.Decrypt(_mdpUtilisateurCrypte).Equals(MdpUtilisateur) ? "1" : "0";
+                }
+
+                // Le compte n'existe pas.
+                return "0";
+            }
+        }
+    }
+}
